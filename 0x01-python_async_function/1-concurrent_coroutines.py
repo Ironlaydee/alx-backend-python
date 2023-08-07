a173705c7ddce418dbfd50e0ec9d2f52db006ae5
Ironlaydee <daintiestdolly@gmail.com>
2023-08-07 13:32:48 +0000

    current routines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,12 +6,12 @@ from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
 async def wait_n(n: int, max_delay: int) -> List[float]:
-"""Spawns wait_random n times with the specified max_delay,
-and returns the list of all the delays (float values)
-"""
+    """Spawns wait_random n times with the specified max_delay,
+    and returns the list of all the delays (float values)
+    """
     futures = [wait_random(max_delay) for _ in range(n)]
     futures = asyncio.as_completed(futures)
     delays = [await future for future in futures]
     return delays
+

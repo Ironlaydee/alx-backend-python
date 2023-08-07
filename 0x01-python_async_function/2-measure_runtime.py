0a2f6c75a9d8b7d348929f495c3042bee28c75d5
Ironlaydee <daintiestdolly@gmail.com>
2023-08-07 19:01:45 +0000

    runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
-
-import asyncio
+""" How to measure the runtime """
 import time
+import asyncio
+from typing import List
 
-async def measure_time(n: int, max_delay: int) -> float:
-    '''Computes the average runtime of wait_n
-    '''
-    wait_n = __import__('1-concurrent_coroutines').wait_n
 
-    total_time = 0.0
-    for _ in range(n):
-        start_time = time.time()
-        await wait_n(1, max_delay)
-        total_time += time.time() - start_time
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
-    return total_time / n
 
-if __name__ == "__main__":
-    n = 5  # Number of times to measure
-    max_delay = 5  # Maximum delay value
-    average_runtime = asyncio.run(measure_time(n, max_delay))
-    print(f"Average runtime per iteration: {average_runtime:.4f} seconds")
+def measure_time(max_delay: int = 10, n: int = 0) -> float:
+    """
+        Args:
+            max_delay: max wait
+            n: spawn function
+
+        Return:
+            float measure time
+    """
+    first_time = time.perf_counter()
+    asyncio.run(wait_n(max_delay, n))
+    elapsed = time.perf_counter() - first_time
+    total_time = elapsed / n
+
+    return total_time
